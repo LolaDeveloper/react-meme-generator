25f3b9e0269659d8370e4339839a97c4ed6d861d
LolaDeveloper <82631715+LolaDeveloper@users.noreply.github.com>
2022-02-21 10:30:10 +0900

    Created App Header

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
+import Troll from "./images/troll-face.png";
 import "./App.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <header className="App-header"></header>
-      <footer>
-        This project was coded by{" "}
-        <a href="#" target="_blank" rel=" noopener norefferer">
-          Funmilola
-        </a>{" "}
-        and is{" "}
-        <a href="#" target="_blank" rel=" noopener norefferer">
-          open-sourced on Github
-        </a>
-      </footer>
+    <div className="  App">
+      <header className="App-header">
+        <img src={Troll} alt="troll face" className="headerImage" />
+        <h2 className="headerTitle">Meme Generator</h2>
+        <small>
+          <h4 className="headerSubTitle"> React course- project 3</h4>
+        </small>
+      </header>
+      <div className="container">
+        <footer>
+          This project was coded by{" "}
+          <a href="#" target="_blank" rel=" noopener norefferer">
+            Funmilola
+          </a>{" "}
+          and is{" "}
+          <a href="#" target="_blank" rel=" noopener norefferer">
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
