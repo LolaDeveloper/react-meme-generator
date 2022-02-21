e625b16e0bef01d894f88818df002f1ede8ee06b
LolaDeveloper <82631715+LolaDeveloper@users.noreply.github.com>
2022-02-21 09:06:05 +0900

    Installed bootstrap and imported CSS and Google fonts into app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header"></header>
+      <footer>
+        This project was coded by{" "}
+        <a href="#" target="_blank" rel=" noopener norefferer">
+          Funmilola
+        </a>{" "}
+        and is{" "}
+        <a href="#" target="_blank" rel=" noopener norefferer">
+          open-sourced on Github
+        </a>
+      </footer>
     </div>
   );
 }
-
-export default App;
