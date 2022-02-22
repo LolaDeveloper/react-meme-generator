72fba155d678639c23b78a0e3111a93e1874862f
LolaDeveloper <82631715+LolaDeveloper@users.noreply.github.com>
2022-02-23 06:48:23 +0900

    Updated App component and styling

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-import Troll from "./images/troll-face.png";
+import Meme from "./components/Meme";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="  App">
       <header className="App-header">
-        <img src={Troll} alt="troll face" className="headerImage" />
+        <img
+          src="../images/troll-face.png"
+          alt="troll face"
+          className="headerImage"
+        />
         <h2 className="headerTitle">Meme Generator</h2>
         <small>
           <h4 className="headerSubTitle"> React course- project 3</h4>
         </small>
       </header>
       <div className="container">
+        <Meme />
         <footer>
           This project was coded by{" "}
           <a href="#" target="_blank" rel=" noopener norefferer">
